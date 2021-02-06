2874f9c9f621fd2c188c7b7f2fed7f4a221a64b2
maheshd-md <maheshd@entrib.com>
2021-02-07 02:02:54 +0530

    Initial project commit

---
+++ spring-security-with-jwt-token-demo/src/main/java/com/example/demo/springsecuritywithjwttokendemo/SpringSecurityWithJwtTokenDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo.springsecuritywithjwttokendemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityWithJwtTokenDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityWithJwtTokenDemoApplication.class, args);
+	}
+
+}
---
+++ spring-security-with-jwt-token-demo/src/test/java/com/example/demo/springsecuritywithjwttokendemo/SpringSecurityWithJwtTokenDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo.springsecuritywithjwttokendemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityWithJwtTokenDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
